75371fb05e0823c627183a27429c3b6cc44b8680
Nouhou Diallo <nouhouprodev@gmail.com>
2025-06-28 18:50:30 +0000

    Refactor l'importation de PrismaClient et amélioration de la gestion des instances globales

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
 
-// add prisma to the NodeJS global types to
-// prevent multiple instances of prisma client
-// get created by hot-reloading in development
 declare global {
-  var prisma: PrismaClient;
+  var prisma: InstanceType<typeof PrismaClient> | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient();
+const prisma = global.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV === "development") {
   global.prisma = prisma;
